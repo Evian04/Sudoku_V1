a0192d9e585a4c289f61e5f72e5c149b9df12f4b
Stallwan <stallwan@proton.me>
2022-11-28 14:11:48 +0100

    Add time measurement

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from src.module.grid import Grid
 from src.module.utils import read_file
+import time
 
 
 content = read_file("model.txt")
 sdk = Grid(content)
+start_time = time.time()
 
 if sdk.solve():
     for line in sdk.get_as():
         print(line)
     print(sdk.is_valid())
+    print(f"{time.time() - start_time} seconds.")
 
 else: quit("Cannot solve sudoku.")
