528a6020569c75cdc33b7b126c478c86f43eed4d
Stallwan <stallwan@proton.me>
2022-11-28 10:05:14 +0100

    Add Euler sudoku

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from src.module.grid import Grid
 from src.module.utils import read_file
 
 
-content = read_file("pbss/model.txt")
+content = read_file("model.txt")
 sdk = Grid(content)
 
 if sdk.solve():
-    print("Sudoku solved !")
     for line in sdk.get_as():
         print(line)
+    print(sdk.is_valid())
 
-else:
-    print("Cannot solve sudoku")
+else: quit("Cannot solve sudoku.")
