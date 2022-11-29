23c97127734604e200266420238a33d35b0ab48b
Stallwan <stallwan@proton.me>
2022-11-29 12:21:59 +0100

    Slight edit

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from src.module.utils import read_file
 import time
 
 
-content = read_file("pbss/model.txt")
+content = read_file("./model.txt")
 sdk = Grid(content)
 start_time = time.time()
 
 if sdk.solve():
     sdk.print_grid()
-    print(sdk.is_valid())
     print(f"{time.time() - start_time} seconds.")
 
 else: quit("Cannot solve sudoku.")
