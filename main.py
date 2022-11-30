ec4341da669da083f73b2136a92c814e0c773c56
Evan <118688666+Evian04@users.noreply.github.com>
2022-11-30 20:51:20 +0100

    function  reworked

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from src.module.utils import read_file
 import time
 
 
-content = read_file("./model.txt")
+path = input("Enter the absuolute path of your sudoku (make sure that there is `/` and not `\\` as file separator)\n-> ")
+content = read_file(path)
 sdk = Grid(content)
 start_time = time.time()
-display = True # `False` to not display the process
+is_process_displayed = False # `False` to not display the process
 
-if sdk.solve(display):
-    if not display:
-        sdk.print_grid()
-    print(f"\n [     {time.time() - start_time} seconds     ]")
+if sdk.solve(is_process_displayed):
+    if not is_process_displayed:
+        sdk.print_grid(False)
+    
+    print(f"\nTime taken : {time.time() - start_time} seconds")
 
 else: quit("Cannot solve sudoku.")
